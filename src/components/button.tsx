6aee4c31c81ae593717d48af3666c34945fed1c6
Jeff Printy <jeff@printy.us>
2024-12-17 14:36:50 -0600

    timing, conditional coloring

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 export default function Button({
   children,
-  onClick
+  onClick,
+  ...properties
 }: {
-  children: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-}) {
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       type='button'
       className='rounded-xl bg-accent-primary px-4 py-2 text-xl transition-colors hover:bg-accent-primary-state focus:bg-accent-primary-state'
       onClick={onClick}
+      {...properties}
     >
       <span>{children}</span>
     </button>
--- src/components/doomsday-quiz.tsx
+++ src/components/doomsday-quiz.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
-import { DateTime } from 'luxon';
+import { PieChart } from 'react-minimal-pie-chart';
+import clsx from 'clsx';
+import { DateTime, Interval } from 'luxon';
 import randomInt from 'random-int';
 
 import Button from './button';
@@ -14,11 +15,18 @@ const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const DoomsdayQuiz = () => {
   const [dt, setDt] = useState<DateTime>();
+  const [startTime, setStartTime] = useState<DateTime>();
+  const [answerTimes, setAnswerTimes] = useState<Array<[number, boolean]>>([]);
   const [correctDay, setCorrectDay] = useState<string>('');
-  const generateNumber = () => {
+  const [correctIncorrect, setCorrectIncorrect] = useState<[number, number]>([0, 0]);
+  const [enableDayClick, setEnableDayClick] = useState(false);
+
+  const generateRandomDate = () => {
     const randomNumber = randomInt(0, 365);
     const randomDate = startDate.plus({ days: randomNumber });
+    setStartTime(DateTime.now());
     setDt(randomDate);
+    setEnableDayClick(true);
     setCorrectDay('');
   };
 
@@ -26,30 +34,63 @@ const DoomsdayQuiz = () => {
     if (!dt) {
       return;
     }
-    // setShowDay(false);
-    // const randomNum = randomInt(0, 365);
-    // const randomDate = startDate.plus({ days: randomNum });
-    // setDt(randomDate);
     const dayShortName = dt.toFormat('ccc');
     const correctDayGuessed = dayShortName === day;
-    console.log('correctDayGuessed', dayShortName, day, correctDayGuessed);
+    setCorrectIncorrect(([correct, incorrect]) => {
+      if (correctDayGuessed) {
+        return [correct + 1, incorrect] as const;
+      }
+      return [correct, incorrect + 1] as const;
+    });
+    if (startTime) {
+      const interval = Interval.fromDateTimes(startTime, DateTime.now());
+      const intervalInSeconds = interval.length('seconds');
+      if (intervalInSeconds) {
+        setAnswerTimes((previous) => [...previous, [intervalInSeconds, correctDayGuessed]]);
+      }
+    }
     setCorrectDay(dayShortName);
   };
 
+  const [correctValue, incorrectValue] = correctIncorrect;
+
   return (
-    <section className='container flex h-72 w-1/3 flex-col items-center justify-between rounded-xl border border-tertiary bg-secondary py-10'>
+    <section className='container flex h-full w-full flex-col items-center justify-between rounded-xl border border-tertiary bg-secondary py-10 lg:w-1/3'>
       <div className='flex h-20 w-full flex-col items-center justify-center bg-gray-600 text-center'>
-        {!!dt && <h2 className='text-5xl'>{dt.toFormat('MMMM dd, yyyy')}</h2>}
+        {!!dt && <h2 className='text-4xl'>{dt.toFormat('MMMM dd, yyyy')}</h2>}
       </div>
-      <div className='grid grid-cols-7'>
+      <div className='grid w-full grid-cols-7'>
         {daysOfWeek.map((day) => (
-          <Button key={day} onClick={() => handleDayClick(day)}>
+          <Button disabled={!enableDayClick} key={day} onClick={() => handleDayClick(day)}>
             {day} {correctDay === day && '✅'}
           </Button>
         ))}
       </div>
       <div className='flex-row items-center justify-center'>
-        <Button onClick={generateNumber}>click</Button>
+        <Button onClick={generateRandomDate}>click</Button>
+      </div>
+      <div className='grid w-full grid-cols-2 flex-row items-center justify-between'>
+        <div className='p-8'>
+          <PieChart
+            label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
+            data={[
+              { title: 'Correct', value: correctValue, color: '#66FF66' },
+              { title: 'Incorrect', value: incorrectValue, color: '#C13C37' }
+            ]}
+          />
+        </div>
+        <div className='flex h-64 flex-col justify-start overflow-y-auto p-8'>
+          <ul>
+            {[...answerTimes].reverse().map(([timeInSeconds, isCorrect]) => (
+              <li
+                className={clsx(isCorrect ? 'bg-green-600' : 'bg-red-600')}
+                key={`${timeInSeconds}_${isCorrect}`}
+              >
+                {timeInSeconds}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
