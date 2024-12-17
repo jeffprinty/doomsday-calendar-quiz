d57d51b34a03e282368c3ab98021ec552d74c0ce
Jeff Printy <jeff@printy.us>
2024-12-17 12:56:49 -0600

    builds

--- src/components/doomsday-quiz.tsx
+++ src/components/doomsday-quiz.tsx
@@ -1,46 +1,47 @@
-import { Component, useState } from 'react';
+import React, { useState } from 'react';
+
 import { DateTime } from 'luxon';
 import randomInt from 'random-int';
 
 //ES6 const, let
-//ES6 Destructuring 
+//ES6 Destructuring
 
 const startDate = DateTime.fromISO('2025-01-01');
 
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const ChildComponent = () => {
-  const [dt, setDt] = useState<DateTime | null>(null)
-  const [correctDay, setCorrectDay] = useState<string>('')
+const DoomsdayQuiz = () => {
+  const [dt, setDt] = useState<DateTime | null>(null);
+  const [correctDay, setCorrectDay] = useState<string>('');
   const [showDay, setShowDay] = useState(false);
   const generateNumber = () => {
     setShowDay(false);
-    const randomNum = randomInt(0, 365);
-    const randomDate = startDate.plus({ days: randomNum });
+    const randomNumber = randomInt(0, 365);
+    const randomDate = startDate.plus({ days: randomNumber });
     setDt(randomDate);
     setCorrectDay('');
   };
-  
+
   const handleDayClick = (day: string) => {
+    if (!dt) {
+      return;
+    }
     // setShowDay(false);
     // const randomNum = randomInt(0, 365);
     // const randomDate = startDate.plus({ days: randomNum });
     // setDt(randomDate);
     const dayShortName = dt.toFormat('ccc');
     const correctDayGuessed = dayShortName === day;
-    console.log('correctDayGuessed', dayShortName, day);
+    console.log('correctDayGuessed', dayShortName, day, correctDayGuessed);
     setCorrectDay(dayShortName);
-    
   };
-  
+
   return (
     <div>
       <div>{!!dt && dt.toFormat('MMMM dd')}</div>
       <div>{!!showDay && !!dt && dt.toFormat('EEEE')}</div>
       <br />
-      <button onClick={generateNumber}>
-        click
-      </button>
+      <button onClick={generateNumber}>click</button>
       <br />
       <br />
       <div>
@@ -49,21 +50,9 @@ const ChildComponent = () => {
             {day} {correctDay === day && '✅'}
           </button>
         ))}
-       </div>
+      </div>
     </div>
   );
-}
+};
 
-const generateDate = () => {};
-
-class App extends Component {
-  render(){
-    return (
-      <div>
-        <ChildComponent/>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector(".container"));
+export default DoomsdayQuiz
