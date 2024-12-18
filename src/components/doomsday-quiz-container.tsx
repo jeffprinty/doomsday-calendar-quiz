48de3bc8ad4819297db29b69eaa7c78a2e996696
Jeff Printy <jeff@printy.us>
2024-12-18 00:32:15 -0600

    mnemonics wip

--- src/components/doomsday-quiz-container.tsx
+++ src/components/doomsday-quiz-container.tsx
@@ -12,6 +12,113 @@ const getRandomDateInYear = (year: number) => {
   return initYearStart.plus({ days: randomNumber });
 };
 
+// const generateYearWithMetadataFromDate = ()
+
+const sharedMemeticHandles = {
+  doubles: '4/4, 6/6, 8/8, 10/10, 12/12',
+  nineToFive: '9-to-5 at 7-11'
+};
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const mnemonics = [
+  {
+    month: 'jan',
+    monthName: 'January',
+    common: 3,
+    leap: 4
+  },
+  {
+    month: 'feb',
+    monthName: 'February',
+    common: 28,
+    leap: 29
+  },
+  {
+    month: 'mar',
+    monthName: 'March',
+    common: 14,
+    leap: 14,
+    memeticHandle: 'pi day'
+  },
+  {
+    month: 'apr',
+    monthName: 'April',
+    common: 4,
+    leap: 4,
+    memeticHandle: sharedMemeticHandles.doubles
+  },
+  {
+    month: 'may',
+    monthName: 'May',
+    common: 9,
+    leap: 9,
+    memeticHandle: sharedMemeticHandles.nineToFive
+  },
+  {
+    month: 'jun',
+    monthName: 'June',
+    common: 6,
+    leap: 6,
+    memeticHandle: sharedMemeticHandles.doubles
+  },
+  {
+    month: 'jul',
+    monthName: 'July',
+    common: 11,
+    leap: 11,
+    memeticHandle: sharedMemeticHandles.nineToFive
+  },
+  {
+    month: 'aug',
+    monthName: 'August',
+    common: 8,
+    leap: 8,
+    memeticHandle: sharedMemeticHandles.doubles
+  },
+  {
+    month: 'sep',
+    monthName: 'September',
+    common: 5,
+    leap: 5,
+    memeticHandle: sharedMemeticHandles.nineToFive
+  },
+  {
+    month: 'oct',
+    monthName: 'October',
+    common: 10,
+    leap: 10,
+    memeticHandle: sharedMemeticHandles.doubles
+  },
+  {
+    month: 'nov',
+    monthName: 'November',
+    common: 7,
+    leap: 7,
+    memeticHandle: sharedMemeticHandles.nineToFive
+  },
+  {
+    month: 'dec',
+    monthName: 'December',
+    common: 12,
+    leap: 12,
+    memeticHandle: sharedMemeticHandles.doubles
+  }
+];
+
 // I want to be able to feed it random dates OR feed it a list of previously incorrect guesses
 const DoomsdayQuizContainer = () => {
   const initYear = 2025;
@@ -72,8 +179,20 @@ const DoomsdayQuizContainer = () => {
           element={
             <PageContainer>
               <div className='explainer'>
-                someone's probably built this already
-                yep, shoutout https://github.com/mkmik/doom/blob/main/src/routes/%2Bpage.svelte
+                {mnemonics.map(({ monthName, common }, index) => {
+                  const hackyMonthNumber = index + 1;
+                  return (
+                    <div
+                      key={monthName}
+                      className='flex flex-row items-center justify-between text-center'
+                    >
+                      {monthName}
+                      <div className=''>
+                        {hackyMonthNumber}/{common}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </PageContainer>
           }
