0fb9dd726d12a0c7bf6b7e2c267fd748c052dc81
Jeff Printy <jeff@printy.us>
2024-12-29 19:54:12 -0600

    OffsetGuesser

--- src/guess-only-date.tsx
+++ src/guess-only-date.tsx
@@ -51,6 +51,49 @@ const GuessOnlyDate = () => {
 
   return (
     <div id='page__guess-only-date' className='w-full md:w-2/3 lg:w-96'>
+      <OffsetGuesser
+        handleClick={handleGuess}
+        guessingDate={guessingDate}
+        lastAnswerCorrect={lastAnswerCorrect}
+      />
+      <Button onClick={getNewGuess} className='my-2 h-16 w-full'>
+        Random Date
+      </Button>
+    </div>
+  );
+};
+
+export default GuessOnlyDate;
+
+const OffsetGuesser = ({
+  guessingDate,
+  handleClick,
+  lastAnswerCorrect
+}: {
+  guessingDate: DateTime;
+  handleClick: (clicked: number) => void;
+  lastAnswerCorrect?: boolean;
+}) => {
+  const showDay = false;
+
+  const mnemonicForMonth = mnemonics.find(({ monthNumber }) => monthNumber === guessingDate.month);
+  console.log('mnemonicForMonth', mnemonicForMonth);
+  if (!mnemonicForMonth) {
+    return <></>;
+  }
+  const doomsdayInMonth = DateTime.fromObject({
+    year: guessingDate.year,
+    month: guessingDate.month,
+    day: mnemonicForMonth.common
+  });
+  const fullOffset = guessingDate.day - doomsdayInMonth.day;
+  const correctOffset = fullOffset % 7;
+
+  // there is almost certainly a sexier way to do this math
+  const alternateCorrectOffset = correctOffset > 0 ? correctOffset - 7 : 7 + correctOffset;
+
+  return (
+    <div id='offset-guesser'>
       <GuessDisplay
         questionText={lastAnswerCorrect ? 'Correct! The offset for' : 'What is the day offset for:'}
         guessText={guessingDate.toFormat('MMMM dd')}
@@ -77,22 +120,18 @@ const GuessOnlyDate = () => {
         {[-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6].map((offset) => (
           <Button
             className={clsx([
-              'mx-1 h-20 px-3 text-center'
+              'h-20 px-3 text-center'
               // incorrectSelection && thisDayWasSelected && 'disabled:bg-red-900',
               // thisDayIsCorrect && 'active:text-black disabled:bg-green-600 disabled:text-black'
             ])}
             key={offset}
-            onClick={() => handleGuess(offset)}
+            onClick={() => handleClick(offset)}
           >
             {offset}
+            {showDay && <div className='text-xs'>day</div>}
           </Button>
         ))}
       </div>
-      <Button onClick={getNewGuess} className='my-2 h-16 w-full'>
-        Random Date
-      </Button>
     </div>
   );
 };
-
-export default GuessOnlyDate;
