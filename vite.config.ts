1a3d5cbbb0cab11fbfcd609e265ab9af4c2b8268
Jeff Printy <jeff@printy.us>
2025-06-30 01:33:43 -0500

    remove partytown, flatCOnfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from 'node:path';
 
-import { partytownVite } from '@builder.io/partytown/utils';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
@@ -12,12 +11,10 @@ export default defineConfig({
     checker({
       typescript: true,
       eslint: {
+        useFlatConfig: true,
         lintCommand: 'eslint --ext ts --ext tsx ./src',
       },
     }),
-    partytownVite({
-      dest: path.join(__dirname, 'dist', '~partytown')
-    })
   ],
   base: '/doomsday-calendar-quiz',
   css: {
