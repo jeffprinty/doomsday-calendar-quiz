867408e183a5fff6b71749851d8db74c115bdebc
Jeff Printy <jeff@printy.us>
2025-07-02 18:03:57 -0500

    doomsyear equation changes

--- src/components/doomsyear-equation.tsx
+++ src/components/doomsyear-equation.tsx
@@ -1,10 +1,22 @@
-import { commonStyles } from '../common';
-import { isOdd } from '../math/basic';
-import { getAnchorDay } from '../math/century';
-import { oddPlusElevenFull } from '../math/doomsyear-odd-plus-eleven';
-import { splitYearIntoComponents } from '../math/year';
+import clsx from 'clsx';
 
-export const DoomsyearEquation = ({ fullYear }: { fullYear: number }) => {
+import { commonStyles } from '../../common';
+import { isOdd } from '../../math/basic';
+import { getAnchorDay } from '../../math/century';
+import { oddPlusElevenFull } from '../../math/doomsyear-odd-plus-eleven';
+import { splitYearIntoComponents } from '../../math/year';
+
+const spacer = <span className='px-2 text-indigo-300'>→</span>;
+
+const DoomsyearEquation = ({
+  className,
+  flat,
+  fullYear,
+}: {
+  className?: string;
+  flat?: boolean;
+  fullYear: number;
+}) => {
   const [century, year] = splitYearIntoComponents(fullYear);
   const { firstResult, secondResult, moduloResult, moduloFromSeven } = oddPlusElevenFull(year);
   const yearPadded = year.toString().padStart(2, '0');
@@ -13,33 +25,46 @@ export const DoomsyearEquation = ({ fullYear }: { fullYear: number }) => {
   const centuryAnchorDay = getAnchorDay(century);
   const addedUp = moduloFromSeven + Number(centuryAnchorDay);
   return (
-    <div className='text-right'>
-      <span className={commonStyles.year}>{yearPadded}</span> {yearIsOdd ? '+ 11 / 2' : '/ 2'} ={' '}
-      <span className={commonStyles.step1}>{firstResult}</span>
-      <br />
+    <div
+      className={clsx('flex', flat ? 'flex-row justify-center' : 'flex-col justify-end', className)}
+    >
+      <div>
+        <span className={commonStyles.year}>{yearPadded}</span> {yearIsOdd ? '+ 11 / 2' : '/ 2'} ={' '}
+        <span className={commonStyles.step1}>{firstResult}</span>
+      </div>
+      {flat && spacer}
       {extraStep && (
-        <>
+        <div>
           <span className={commonStyles.step1}>{firstResult}</span> + 11 ={' '}
           <span className={commonStyles.step2}>{secondResult}</span>
-          <br />
-        </>
+          {flat && spacer}
+        </div>
       )}
-      <span className={extraStep ? commonStyles.step2 : commonStyles.step1}>{secondResult}</span> %
-      7 = <span className={commonStyles.step3}>{moduloResult}</span>
-      <br />7 - <span className={commonStyles.step3}>{moduloResult}</span> ={' '}
-      <span className={commonStyles.step4}>{moduloFromSeven}</span>
-      <br />
-      <span className={commonStyles.step4}>{moduloFromSeven}</span> +{' '}
-      <span className={commonStyles.step5}>{centuryAnchorDay}</span> ={' '}
-      <span className=''>{addedUp}</span>
-      {addedUp > 6 && (
-        <>
-          <br />
-          <span className=''>
-            {addedUp} % 7 = {addedUp % 7}
-          </span>
-        </>
-      )}
+      <div>
+        <span className={extraStep ? commonStyles.step2 : commonStyles.step1}>{secondResult}</span>{' '}
+        % 7 = <span className={commonStyles.step3}>{moduloResult}</span>
+      </div>
+      {flat && spacer}
+      <div>
+        7 - <span className={commonStyles.step3}>{moduloResult}</span> ={' '}
+        <span className={commonStyles.step4}>{moduloFromSeven}</span>
+      </div>
+      {flat && spacer}
+      <div>
+        <span className={commonStyles.step4}>{moduloFromSeven}</span> +{' '}
+        <span className={commonStyles.step5}>{centuryAnchorDay}</span> ={' '}
+        <span className=''>{addedUp}</span>
+        {addedUp > 6 && (
+          <>
+            <br />
+            <span className=''>
+              {addedUp} % 7 = {addedUp % 7}
+            </span>
+          </>
+        )}
+      </div>
     </div>
   );
 };
+
+export default DoomsyearEquation;
